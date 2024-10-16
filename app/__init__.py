68105d5d7a21d2c10f2ebc3620bd727c1aa32959
reeceslade <reecesladey@outlook.com>
2024-10-16 15:26:09 +0100

    Added flask-admin as a GUI to interact with database tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, g, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
 from app.config import Config
+from flask_admin.contrib.sqla import ModelView
 import logging
 import sys
 
@@ -14,5 +16,16 @@ logging.basicConfig(filename='app/logs/service.log', level=logging.DEBUG,
 db = SQLAlchemy(app)
 # db.Model.metadata.reflect(db.engine)
 migrate = Migrate(app, db)
+admin = Admin(app, name='TableReservationSystem', template_mode='bootstrap4')
+#Admin views go here 
+
+from app.models import Seating, Booking_slots #, bookings
+# Add model views here
+
+# Add the models to the admin interface
+admin.add_view(ModelView(Seating, db.session))
+admin.add_view(ModelView(Booking_slots, db.session))
+#admin.add_view(ModelView(bookings, db.session))
+
 
 from app import routes, models
